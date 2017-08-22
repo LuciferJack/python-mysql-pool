5354664617f47e37086848f7d0a7d791c3f89aaf
lujunxu <lujunxu@baidu.com>
2017-08-22 19:22:58 +0800

    project merge

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author = 'Lucifer Jack',
     author_email = 'lujunxucuc@gmail.com',
     url = 'https://codeload.github.com/LuciferJack/python-mysql-pool/zip/master', # use the URL to the github repo
-    download_url = 'https://github.com/LuciferJack/python-mysql-pool/archive/0.1.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/LuciferJack/python-mysql-pool/archive/0.2.tar.gz', # I'll explain this in a second
     keywords = ['mysql pool', 'queue', 'support list'], # arbitrary keywords
     classifiers = [],
 )
