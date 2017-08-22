6a8a5e35426eb25659036361aaecfa2c89ab37d4
lujunxu <lujunxu@baidu.com>
2017-08-22 19:29:34 +0800

    project pyc

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name = 'python-mysql-pool',
     packages = ['python-mysql-pool'], # this must be the same as the name above
     version = '0.1',
-    description = 'python practical mysql pool -motivation from=>[lost connection to MySQL server during query]',
+    description = 'python practical mysql pool -motivation from=>[lost connection to MySQL server during query]  base on mysql-connector ',
     author = 'Lucifer Jack',
     author_email = 'lujunxucuc@gmail.com',
     url = 'https://codeload.github.com/LuciferJack/python-mysql-pool/zip/master', # use the URL to the github repo
