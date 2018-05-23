2a2dad5d5e267e93be238303c3fc2d28a02473c4
lujunxu <lujunxu@baidu.com>
2018-05-23 14:43:10 +0800

    add dependency

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.6',
+    version='1.0.7',
 
     description='python practical mysql pool -motivation from=>[lost connection to MySQL server during query]  base on mysql-connector '
                 'support fixed | dynamic pool',
@@ -95,6 +95,9 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
+    setup_requires=[
+        'wheel',
+    ],
 
      install_requires=[
          'wheel',
